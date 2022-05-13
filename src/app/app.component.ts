02982c8b02731999fe109dcdaf1eb186945e5e99
Jamal <jamal72e@gmail.com>
2022-05-13 09:30:29 +0430

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Cards';
+  posts = [{
+    title: 'درخت',
+    imageUrl: 'assets/images/ic-zoom (2).png',
+    username: 'مستر درخت',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. i saw neat tree',
+  },
+  {
+    title: 'کوه',
+    imageUrl: 'assets/images/ic-zoom.png',
+    username: 'مستر کوه',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. i saw neat mountain',
+  },
+  {
+    title: 'دوچرخه',
+    imageUrl: 'assets/images/ic-zoom1.png',
+    username: 'مستر دوچرخه',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. i saw neat biking',
+  }];
+
+  getPosts(){
+    return this.posts;
+  }
+
 }
